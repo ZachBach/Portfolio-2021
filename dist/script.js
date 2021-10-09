3c0d001f42d249ad3c99eb1fddc36979dd8aa68d
Zach Auerbach <auernachzach@gmail.com>
2021-10-08 18:25:37 -0700

    it still doesn't work even maliciously lol

--- dist/script.js
+++ dist/script.js
@@ -150,17 +150,14 @@ const audio = document.querySelectorAll(".audio")
 
 
 const musicPlayer = () => {
-    let music = confirm("Would you like to hear music?");
-    
-    if (music) {
-        audio.play();
-        console.log("music")
-    } else {
-        console.log("Only Music lovers allowed")
-        audio.muted = true;
-        audio.pause();
-    }
+    audio.muted = true;
+    audio.muted = false;
+    console.log(audio)
+   if (audio.muted === true) {
+       window.location.reload();
+   }
 }
+
 
 musicPlayer();
 
